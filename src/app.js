36d10e7890efd77d8bf7d140c07dc7b8fc6bd7a6
frankirova <franciscor1905@gmail.com>
2023-02-14 23:31:06 -0300

    Conectando backend PRUEBA FIREBASE-ADMIN

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { auth } = require("./firebase");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(require('./routes/index'))
+app.use(cors());
+
+app.use(require("./routes/index"));
+
 module.exports = app;
